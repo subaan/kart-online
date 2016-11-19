00e78cf686d73bb5df101788886262b445397bb7
Abdul Suban <abdulsubaanbtech@gmail.com>
2016-11-19 20:53:04 +0530

    Commented ant plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,8 +50,8 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.27'
         // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
-        compile 'org.apache.ant:ant:1.7.1'
-        compile 'org.apache.ant:ant-launcher:1.7.1'
+//        compile 'org.apache.ant:ant:1.7.1'
+//        compile 'org.apache.ant:ant-launcher:1.7.1'
     }
 
     plugins {
